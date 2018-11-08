2131a5bc83b50a80fe9c44423a46a6c6a2e1e973
aboud20-meet <aboud20@meet.mit.edu>
2018-11-08 18:49:25 +0200

    g

--- database.py
+++ database.py
@@ -16,3 +16,7 @@ def create_cat(name):
 def get_all_cats():
     cats = session.query(Cat).all()
     return cats
+
+def get_cat(id):
+    cat_object = session.query(Cat).filter_by(id=id).first()
+    return cat_object    
